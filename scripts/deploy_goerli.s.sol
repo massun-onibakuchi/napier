1d1614195ab297cfb5255a1f582f15f68a1b0f1d
massun-onibakuchi <etherdao0x00@gmail.com>
2022-09-24 20:35:33 +0900

    fix compound adapter

--- scripts/deploy_goerli.s.sol
+++ scripts/deploy_goerli.s.sol
@@ -13,50 +13,32 @@ import "forge-std/Script.sol";
 contract Deploy is Script {
     uint256 internal constant DELTA = 150;
 
-    address internal constant DAI_MAINNET = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address internal constant WETH_MAINNET = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant WETH_GOERLI = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    address internal constant DAI_GOERLI = 0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844;
 
     // compound
-    address internal constant CDAI_MAINNET = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
     address internal constant CDAI_GOERLI = 0x0545a8eaF7ff6bB6F708CbB544EA55DBc2ad7b2a;
-    address internal constant CETH_MAINNET = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
-
-    // yearn
-    address internal constant YDAI_MAINNET = 0xdA816459F1AB5631232FE5e97a05BBBb94970c95;
+    address internal constant CETH_GOERLI = 0x64078a6189Bf45f80091c6Ff2fCEe1B15Ac8dbde;
 
     // aave v2
-    address internal constant ADAI_MAINNET = 0x028171bCA77440897B824Ca71D1c56caC55b68A3;
     address internal constant ADAI_GOERLI = 0x31f30d9A5627eAfeC4433Ae2886Cf6cc3D25E772;
-
-    address internal constant LENDING_POOL_V2_MAINNET = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
     address internal constant LENDING_POOL_V2_GOERLI = 0x4bd5643ac6f66a5237E18bfA7d47cF22f1c9F210;
 
     function run() public {
         uint256 feePst = 0;
         uint256 maturity = block.timestamp + 10 weeks;
-        address underlying = DAI_MAINNET;
+        address underlying = DAI_GOERLI;
 
         // Foundry supports various wallet options.
         // https://book.getfoundry.sh/reference/forge/forge-script#wallet-options---raw
         vm.startBroadcast();
 
-        Adapter[] memory adapters = new Adapter[](3);
+        Adapter[] memory adapters = new Adapter[](2);
 
         Adapter cAdapter = new CompoundAdapter(
             Adapter.AdapterParams({
-                underlying: DAI_MAINNET,
+                underlying: DAI_GOERLI,
                 target: CDAI_MAINNET,
-                delta: DELTA,
-                minm: 0,
-                maxm: 0,
-                issuanceFee: feePst
-            })
-        );
-
-        Adapter yAdapter = new YearnAdapter(
-            Adapter.AdapterParams({
-                underlying: DAI_MAINNET,
-                target: YDAI_MAINNET,
                 delta: DELTA,
                 minm: 0,
                 maxm: 0,
@@ -66,7 +48,7 @@ contract Deploy is Script {
 
         Adapter aAdapter = new AaveV2Adapter(
             Adapter.AdapterParams({
-                underlying: DAI_MAINNET,
+                underlying: DAI_GOERLI,
                 target: ADAI_MAINNET,
                 delta: DELTA,
                 minm: 0,
@@ -77,8 +59,7 @@ contract Deploy is Script {
         );
 
         adapters[0] = cAdapter;
-        adapters[1] = yAdapter;
-        adapters[2] = aAdapter;
+        adapters[1] = aAdapter;
 
         NapierPoolFactory poolFactory = new NapierPoolFactory(address(this));
         Tranche tranche = new Tranche(adapters, IERC20Metadata(underlying), maturity, msg.sender, poolFactory);
--- src/tests/adapters/compound/TestCompoundAdapterCDAI.t.sol
+++ src/tests/adapters/compound/TestCompoundAdapterCDAI.t.sol
@@ -16,6 +16,9 @@ contract TestCompoundAdapterCDAI is TestAdapter {
     address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address internal constant cDAI = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
 
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant CETH = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
+
     function _setup() internal override {
         feePst = 0;
         maturity = block.timestamp + 10 weeks;
@@ -23,7 +26,9 @@ contract TestCompoundAdapterCDAI is TestAdapter {
         target = cDAI;
 
         adapter = new CompoundAdapter(
-            Adapter.AdapterParams({underlying: DAI, target: cDAI, delta: DELTA, minm: 0, maxm: 0, issuanceFee: feePst})
+            Adapter.AdapterParams({underlying: DAI, target: cDAI, delta: DELTA, minm: 0, maxm: 0, issuanceFee: feePst}),
+            WETH,
+            CETH
         );
 
         Adapter[] memory adapters = new Adapter[](1);
